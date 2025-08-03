b3f980e2fb95213e756c2a23e49e96814cf1eac4
yosuahres <haresyosuaa@gmail.com>
2025-08-03 13:13:23 +0700

    feat: add loading screen

---
+++ src/app/loading.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-black">
+      <div className="relative">
+        <div className="animate-heartbeat">
+          <svg
+            className="h-24 w-24 text-red-500"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
+            <path
+              fillRule="evenodd"
+              d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
